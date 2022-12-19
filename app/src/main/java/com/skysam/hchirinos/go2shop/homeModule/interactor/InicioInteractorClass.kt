9d9f7996dd4d5c7ab848ffcd83bef09414097e5f
Chirinos <inversionesskysam@gmail.com>
2022-12-19 17:06:00 -0400

    V7.1 Ajuste en lectura de tasa diaria/Liberacion Play Store

--- app/src/main/java/com/skysam/hchirinos/go2shop/homeModule/interactor/InicioInteractorClass.kt
+++ app/src/main/java/com/skysam/hchirinos/go2shop/homeModule/interactor/InicioInteractorClass.kt
@@ -6,7 +6,14 @@ import com.skysam.hchirinos.go2shop.database.sharedPref.SharedPreferenceBD
 import com.skysam.hchirinos.go2shop.homeModule.presenter.InicioPresenter
 import kotlinx.coroutines.*
 import org.jsoup.Jsoup
+import java.security.KeyManagementException
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import java.util.*
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 import kotlin.coroutines.CoroutineContext
 
 /**
@@ -25,7 +32,7 @@ class InicioInteractorClass(private val inicioPresenter: InicioPresenter): Inici
 
             withContext(Dispatchers.Default) {
                 try {
-                    val doc = Jsoup.connect(url).get()
+                    val doc = Jsoup.connect(url).sslSocketFactory(socketFactory()).get()
                     val data = doc.select("div#dolar")
                     valor = data.select("strong").last()?.text()
                 } catch (e: Exception) {
@@ -44,6 +51,35 @@ class InicioInteractorClass(private val inicioPresenter: InicioPresenter): Inici
                 inicioPresenter.resultSync(true)
             } else {
                 inicioPresenter.resultSync(false)
+            }
+        }
+    }
+
+    private fun socketFactory(): SSLSocketFactory {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            @Throws(CertificateException::class)
+            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+            }
+
+            @Throws(CertificateException::class)
+            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                return arrayOf()
+            }
+        })
+
+        try {
+            val sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+            return sslContext.socketFactory
+        } catch (e: Exception) {
+            when (e) {
+                is RuntimeException, is KeyManagementException -> {
+                    throw RuntimeException("Failed to create a SSL socket factory", e)
+                }
+                else -> throw e
             }
         }
     }
